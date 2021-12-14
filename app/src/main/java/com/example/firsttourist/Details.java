c3a9f21542489bdff4ee687b99bdc25daffe889d
Adarsh Padhi <adarshpadhi14@gmail.com>
2021-12-14 11:51:29 +0530

    current location fetched

--- app/src/main/java/com/example/firsttourist/Details.java
+++ app/src/main/java/com/example/firsttourist/Details.java
@@ -1,60 +1,220 @@
 package com.example.firsttourist;
 
 import static android.location.LocationManager.*;
+import static java.lang.Thread.sleep;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
+import androidx.core.app.ActivityCompat;
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.location.LocationListener;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Looper;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 import com.bumptech.glide.Glide;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 import java.util.Objects;
 
 public class Details extends AppCompatActivity {
+    private LocationRequest locationRequest;
+    private Button googleMap;
+    Double latitude=0.0, longitude=0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-        TextView t2 = (TextView)findViewById(R.id.text1);
+        TextView t2 = (TextView) findViewById(R.id.text1);
         ImageView im = findViewById(R.id.image);
-        RatingBar rb1=findViewById(R.id.detail_rating);
+        RatingBar rb1 = findViewById(R.id.detail_rating);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        googleMap = findViewById(R.id.map);
+
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
-            Intent intent = getIntent();
-            if(intent!=null)
-            {
-                String name = intent.getStringExtra("Name");
-                String description = intent.getStringExtra("Description");
-                String image_url = intent.getStringExtra("Image");
-                String rating = intent.getStringExtra("rating");
+        Intent intent = getIntent();
+        if (intent != null) {
+            String name = intent.getStringExtra("Name");
+            String description = intent.getStringExtra("Description");
+            String image_url = intent.getStringExtra("Image");
+            String rating = intent.getStringExtra("rating");
 
 
-                t2.setText(description);
-                Glide.with(this)
-                        .load(image_url)
-                        .placeholder(R.drawable.ic_launcher_foreground)
-                        .into(im);
-                rb1.setRating(Float.parseFloat(rating));
-                im.setContentDescription(name);
-                this.setTitle(name);
-                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            }
+            t2.setText(description);
+            Glide.with(this)
+                    .load(image_url)
+                    .placeholder(R.drawable.ic_launcher_foreground)
+                    .into(im);
+            rb1.setRating(Float.parseFloat(rating));
+            im.setContentDescription(name);
+            this.setTitle(name);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+            locationRequest = LocationRequest.create();
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            locationRequest.setInterval(5000);
+            locationRequest.setFastestInterval(2000);
+
+            googleMap.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v)
+                {
+                    getCurrentLocation();
+                    String res = "latitude : "+Double.toString(latitude)+"\n longitude: "+Double.toString(longitude);
+                    Toast.makeText(getApplicationContext(),res,Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
 
     }
 
     @Override
-    public boolean onSupportNavigateUp()
-    {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                if (isGPSEnabled()) {
+
+                    getCurrentLocation();
+
+                } else {
+
+                    turnOnGPS();
+                }
+            }
+        }
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 2) {
+            if (resultCode == Activity.RESULT_OK) {
+
+                getCurrentLocation();
+            }
+        }
+    }
+
+    private void getCurrentLocation() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(Details.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+                if (isGPSEnabled()) {
+
+                    LocationServices.getFusedLocationProviderClient(Details.this)
+                            .requestLocationUpdates(locationRequest, new LocationCallback() {
+                                @Override
+                                public void onLocationResult(@NonNull LocationResult locationResult) {
+                                    super.onLocationResult(locationResult);
+
+                                    LocationServices.getFusedLocationProviderClient(Details.this)
+                                            .removeLocationUpdates(this);
+
+                                    if (locationResult != null && locationResult.getLocations().size() > 0) {
+
+                                        int index = locationResult.getLocations().size() - 1;
+                                        latitude = locationResult.getLocations().get(index).getLatitude();
+                                        longitude = locationResult.getLocations().get(index).getLongitude();
+//                                         AddressText.setText("Latitude: "+ latitude + "\n" + "Longitude: "+ longitude);
+                                    }
+                                }
+                            }, Looper.getMainLooper());
+
+                } else {
+                    turnOnGPS();
+                }
+
+            } else {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }
+    }
+
+    private void turnOnGPS() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+
+        Task<LocationSettingsResponse> result = LocationServices.getSettingsClient(getApplicationContext())
+                .checkLocationSettings(builder.build());
+
+        result.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+
+                try {
+                    LocationSettingsResponse response = task.getResult(ApiException.class);
+                    Toast.makeText(Details.this, "GPS is already tured on", Toast.LENGTH_SHORT).show();
+
+                } catch (ApiException e) {
+
+                    switch (e.getStatusCode()) {
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+
+                            try {
+                                ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                                resolvableApiException.startResolutionForResult(Details.this, 2);
+                            } catch (IntentSender.SendIntentException ex) {
+                                ex.printStackTrace();
+                            }
+                            break;
+
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            //Device does not have location
+                            break;
+                    }
+                }
+            }
+        });
+
+    }
+
+    private boolean isGPSEnabled() {
+        LocationManager locationManager = null;
+        boolean isEnabled = false;
+
+        if (locationManager == null) {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        }
+
+        isEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        return isEnabled;
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
     }
